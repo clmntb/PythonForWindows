3a075e927627dca7d0a6037ab0a68cd056a8cf6f
clmntb <clmntbrlnd@gmail.com>
2018-06-04 11:37:55 +0200

    Suppression de la vérification de la version de python sur la gestion des longs pour générer toujours le même fichier

--- ctypes_generation/def_parser.py
+++ ctypes_generation/def_parser.py
@@ -1,4 +1,3 @@
-import sys
 from winstruct import WinStruct, WinStructType, Ptr
 import dummy_wintypes
 from simpleparser import *
@@ -10,7 +9,7 @@ class WinDef(object):
         self.code = code
 
     def generate_ctypes(self):
-        if sys.version_info[0] == 3 and self.code[-1] == "L":
+        if self.code[-1] == "L":
             try:
                 int(self.code[:-1])
             except ValueError:
